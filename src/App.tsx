305562700c6bb42385d47553c169a4024a059791
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-10 01:27:26 +0300

    feat: create build

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+
+  return (
+    <div className="w-full flex justify-center items-center min-h-screen">
+      123
+    </div>
+  );
+};
+
+export default App;
